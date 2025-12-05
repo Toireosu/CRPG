aede5f2c8cc23c736c4b5b6c84d2621f8d665c3a
William Lindgren <william.lindgren19@gmail.com>
2025-12-05 14:22:34 +0100

    Ai start

---
+++ include/data/ai_params.h
@@ -0,0 +1,10 @@
+#ifndef AI_H
+#define AI_H 
+
+#include "entity.h"
+
+typedef struct AIParams {
+    Entity* entity;
+} AIParams;
+
+#endif 
---
+++ include/systems/ai_controller.h
@@ -0,0 +1,8 @@
+#ifndef AI_CONTROLLER_H
+#define AI_CONTROLLER_H 
+
+#include "data/ai_params.h"
+
+void AIController_Tick(AIParams* ai);
+
+#endif
